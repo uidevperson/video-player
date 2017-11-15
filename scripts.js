5b2b53030caa6def925d936ecaf986942a693405
uidevperson <32483505+uidevperson@users.noreply.github.com>
2017-11-15 19:46:43 +0000

    play pause

---
+++ scripts.js
@@ -0,0 +1,25 @@
+/* Get Our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+// functions
+function togglePlay(argument) {
+	const method  = video.paused ? 'play': 'pause';
+	video[method]();
+
+}
+
+function updateButton(){
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+}
+// add event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
